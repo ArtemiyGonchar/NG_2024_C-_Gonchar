7c83404faa8255c7063464340337fa5242a8a7be
ArtemiyGonchar <artmiygonchar@gmail.com>
2024-08-27 15:33:15 +0300

    uploaded hash encoder

---
+++ Task_2_1/Task_2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Task_2_1.Services;
+namespace Task_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoder encoder = new Encoder();
+
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            string hashedPassword = encoder.HashPassword(password);
+            Console.WriteLine(hashedPassword);
+        }
+    }
+}
---
+++ Task_2_1/Task_2_1/Services/Encoder.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+namespace Task_2_1.Services
+{
+    internal class Encoder
+    {
+        public string HashPassword(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha256.ComputeHash(bytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
